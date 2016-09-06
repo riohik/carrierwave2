15e5e244afdd58ec4a253db76078d9a45e4f7cf2
riohik <riohik@gmail.com>
2016-09-06 05:05:19 +0000

    d

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
-  devise_for :users
+  devise_for :users ,:controllers => {
+    :sessions => 'users/sessions'
+  }
   resources :posts do
     member do
       put 'like', to: 'posts#like'
