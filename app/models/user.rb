4adf0b9ccbaa6a204ff0bd88302c955bb28a9383
riohik <riohik@gmail.com>
2016-09-07 07:26:32 +0000

    a

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,8 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   acts_as_voter
-  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable #:validatable
+  def email_required?
+  false
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160905022447) do
+ActiveRecord::Schema.define(version: 20160907050848) do
 
   create_table "comments", force: :cascade do |t|
     t.integer  "post_id"
@@ -56,6 +56,8 @@ ActiveRecord::Schema.define(version: 20160905022447) do
     t.datetime "created_at",                             null: false
     t.datetime "updated_at",                             null: false
     t.boolean  "guest",                  default: false
+    t.string   "username"
+    t.string   "name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
